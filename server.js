887a9254d1a3a2bbd8b595751708be233703710c
BlacklistX20 <a.syafrul.aswar20@gmail.com>
2026-07-20 07:37:29 +0800

    remove modbusService

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const { startPolling } = require("./modbusService");
 
 // Load env variables
 dotenv.config();
@@ -28,6 +27,5 @@ connectDB().then(() => {
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
         console.log(`Server berjalan di port ${PORT}`);
-        startPolling(); // Mulai proses polling data dari Modbus
     });
 });
